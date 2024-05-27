046b6021c200f6957b79ee65c2fdf36eb20ca729
1F10230178 <s1F102301785@iniad.org>
2024-05-27 13:10:53 +0900

    rename file

--- wiki.py
+++ wiki.py
@@ -3,9 +3,4 @@ import os
 
 # htmlを開く
 def wiki():
-    webbrowser.open('C:/Users/iniad/Documents/my_library/html/index.html')
-
-url = 'C:/Users/iniad/Documents/my_library/html/index.html'
-url="https://openai.com"
-name="sample"
-html_link = f'<a href="{url}">{name}</a>'
+    webbrowser.open('C:/Users/iniad/Documents/test_library/html/index.html')
